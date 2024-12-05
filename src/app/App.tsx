ad7a0bde2c916fcd60abb4dcdd7a902aa3f0c8b2
Diego Fleitas <diego.fleitas68@gmail.com>
2024-12-05 01:32:10 -0300

    show all results

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -80,29 +80,36 @@ const App: React.FC = () => {
 
   const handleSearch = async () => {
     const movieTitles = parseInput(query);
-    const detailedMovies = await Promise.all(
-      (Array.isArray(movieTitles) ? movieTitles : [movieTitles]).map(
-        async (title) => {
-          const response = await fetch(
-            `/api/proxy/https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
-              title
-            )}`
-          );
-          const data = await response.json();
-          const movie = data.results[0];
-          if (movie) {
-            const movieDetailsResponse = await fetch(
-              `/api/proxy/https://api.themoviedb.org/3/movie/${movie.id}`
-            );
-            const movieDetails = await movieDetailsResponse.json();
-            return { ...movie, runtime: movieDetails.runtime };
-          }
-          return null;
-        }
-      )
-    );
+    const detailedMovies: Movie[] = [];
 
-    setResults(detailedMovies.filter((movie) => movie !== null) as Movie[]);
+    for (const title of Array.isArray(movieTitles)
+      ? movieTitles
+      : [movieTitles]) {
+      let page = 1;
+      let totalPages = 1;
+
+      while (page <= totalPages) {
+        const response = await fetch(
+          `/api/proxy/https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+            title
+          )}&page=${page}`
+        );
+        const data = await response.json();
+        totalPages = data.total_pages;
+
+        for (const movie of data.results) {
+          const movieDetailsResponse = await fetch(
+            `/api/proxy/https://api.themoviedb.org/3/movie/${movie.id}`
+          );
+          const movieDetails = await movieDetailsResponse.json();
+          detailedMovies.push({ ...movie, runtime: movieDetails.runtime });
+        }
+
+        page++;
+      }
+    }
+
+    setResults(detailedMovies);
   };
 
   return (
@@ -120,11 +127,13 @@ const App: React.FC = () => {
             </button>
           </div>
           <div className={`${styles.results} ${styles['padding-vertical']}`}>
-            <Carousel className={styles.carousel}>
-              {results.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-              ))}
-            </Carousel>
+            <div className={styles.carouselContainer}>
+              <Carousel className={styles.carousel}>
+                {results.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </Carousel>
+            </div>
           </div>
         </header>
       </div>
