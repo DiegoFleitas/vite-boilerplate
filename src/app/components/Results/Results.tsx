909fa0b6b57b68fad80889fdc92353ae9ae8ae68
Diego Fleitas <diego.fleitas68@gmail.com>
2024-12-12 16:45:31 -0300

    fix results showing incorrect index on carousel

--- src/app/components/Results/Results.tsx
+++ src/app/components/Results/Results.tsx
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import MovieCard from '../MovieCard/MovieCard';
 import { Movie } from '../../types';
 import { Box, Container } from '@mui/material';
 
-const Results: React.FC<{ results: Movie[][] }> = ({ results }) => (
-  <Container sx={{ paddingY: 4 }}>
-    {results.map((movies, index) => (
-      <Box key={index} sx={{ marginBottom: 4 }}>
-        <Carousel>
-          {movies.map((movie) => (
-            <MovieCard key={movie.id} movie={movie} />
-          ))}
-        </Carousel>
-      </Box>
-    ))}
-  </Container>
-);
+const Results: React.FC<{ results: Movie[][] }> = ({ results }) => {
+  const [carouselIndices, setCarouselIndices] = useState<number[]>([]);
+
+  useEffect(() => {
+    // Always set the first carousel index to 0 when results change
+    setCarouselIndices((prevIndices) => {
+      const newIndices = [0, ...prevIndices];
+      return newIndices.slice(0, results.length);
+    });
+  }, [results]);
+
+  const handleCarouselChange = (index: number, carouselIndex: number) => {
+    setCarouselIndices((prevIndices) => {
+      const newIndices = [...prevIndices];
+      newIndices[carouselIndex] = index;
+      return newIndices;
+    });
+  };
+
+  return (
+    <Container sx={{ paddingY: 4 }}>
+      {results.map((movies, carouselIndex) => (
+        <Box key={carouselIndex} sx={{ marginBottom: 4 }}>
+          <Carousel
+            selectedItem={carouselIndices[carouselIndex]}
+            onChange={(index) => handleCarouselChange(index, carouselIndex)}
+          >
+            {movies.map((movie) => (
+              <MovieCard key={movie.id} movie={movie} />
+            ))}
+          </Carousel>
+        </Box>
+      ))}
+    </Container>
+  );
+};
 
 export default Results;
