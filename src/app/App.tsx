2658c590a474c053d15efaea033011596499456a
Diego Fleitas <diego.fleitas68@gmail.com>
2024-12-20 11:59:53 -0300

    to boilerplate

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,190 +1,105 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { parseInput } from './utils';
-import SearchBar from './components/SearchBar/SearchBar';
-import Results from './components/Results/Results';
-import { Movie } from './types';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {
-  Container,
+  AppBar,
+  Toolbar,
   Typography,
+  Container,
   Button,
-  Box,
-  Snackbar,
-  Alert,
+  Link,
+  IconButton,
 } from '@mui/material';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Notice from './components/Notice/Notice';
+import MenuIcon from '@mui/icons-material/Menu';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import Welcome from './components/Welcome/Welcome';
+import Sidebar from './components/Sidebar/Sidebar';
+import styles from './App.module.css';
 
-const App: React.FC = () => {
-  const [query, setQuery] = useState<string>('');
-  const [results, setResults] = useState<Movie[][]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [showNotification, setShowNotification] = useState<boolean>(false);
-  const [googleSearchLink, setGoogleSearchLink] = useState<string>('');
-  const [notice, setNotice] = useState<string>('');
+function App(): JSX.Element {
+  const [count, setCount] = useState<number>(0);
+  const [drawerOpen, setDrawerOpen] = useState<boolean>(false);
 
-  const MAX_MOVIES = 15;
-
-  const handleSearch = async () => {
-    setIsLoading(true);
-    const movieTitles = parseInput(query);
-    const detailedMovies: Movie[] = [];
-
-    const fetchMovieDetails = async (movieId: number) => {
-      const response = await fetch(
-        `/api/proxy/https://api.themoviedb.org/3/movie/${movieId}&include_adult=true`
-      );
-      return response.json();
-    };
-
-    const fetchMovies = async (title: string) => {
-      let page = 1;
-      let totalPages = 1;
-      const movies: Movie[] = [];
-
-      while (page <= totalPages && movies.length < MAX_MOVIES) {
-        const response = await fetch(
-          `/api/proxy/https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
-            title
-          )}&page=${page}&include_adult=true`
-        );
-        const data = await response.json();
-        totalPages = data.total_pages;
-
-        if (data.results.length === 0) {
-          setGoogleSearchLink(
-            `https://www.google.com/search?q=${encodeURIComponent(title)}`
-          );
-          setShowNotification(true);
-          setIsLoading(false);
-          return [];
-        }
-
-        movies.push(...data.results);
-        page++;
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
       }
-
-      return movies.slice(0, MAX_MOVIES);
+      setDrawerOpen(open);
     };
-
-    const movieTitlesArray = Array.isArray(movieTitles)
-      ? movieTitles
-      : [movieTitles];
-    const moviePromises = movieTitlesArray.map((title) => fetchMovies(title));
-    const moviesArray = await Promise.all(moviePromises);
-
-    const allMovies = moviesArray.flat().slice(0, MAX_MOVIES);
-    const movieDetailsPromises = allMovies.map((movie) =>
-      fetchMovieDetails(movie.id)
-    );
-    const movieDetailsArray = await Promise.all(movieDetailsPromises);
-
-    movieDetailsArray.forEach((details, index) => {
-      detailedMovies.push({ ...allMovies[index], runtime: details.runtime });
-    });
-
-    setResults((prevResults) => [detailedMovies, ...prevResults]);
-    setIsLoading(false);
-  };
-
-  const handleCloseNotification = () => {
-    setShowNotification(false);
-  };
-
-  const handleOpenGoogle = () => {
-    window.open(googleSearchLink, '_blank');
-    setShowNotification(false);
-  };
-
-  const getNotice = () => {
-    const notices: { [key: string]: string } = {
-      Monday: 'HOY - Inicio nominaciones',
-      Tuesday: 'HOY - Inicio votación',
-      Wednesday: 'HOY - Continúa votación',
-      Thursday: 'HOY - Cierre votacion (23:59hrs)',
-      Friday: 'HOY - Proyeccion película ganadora (19:30hrs)',
-      Saturday: 'HOY - Disfrutá el finde!',
-      Sunday: 'HOY - Disfrutá el finde!',
-    };
-    const today = new Date();
-    const dayName: string = today.toLocaleDateString('en-US', {
-      weekday: 'long',
-    });
-    return notices[dayName] || '';
-  };
-
-  useEffect(() => {
-    setNotice(getNotice());
-  }, []);
 
   return (
     <Router>
-      <Container
-        sx={{ width: '100vw', padding: 0, margin: 0, maxWidth: '100vw' }}
-      >
-        <Notice message={notice} />
-        <Box
-          sx={{
-            backgroundColor: '#282c34',
-            minHeight: '100vh',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            color: 'white',
-            padding: '20px 0',
-            width: '100vw',
-          }}
-        >
-          <Typography
-            variant="h1"
-            component="h1"
-            sx={{
-              marginBottom: '20px',
-              fontSize: { xs: '2rem', sm: '3rem', md: '4rem', lg: '5rem' },
-            }}
-          >
-            Peli OK?
-          </Typography>
-          <Box sx={{ width: '100%', maxWidth: '600px' }}>
-            <SearchBar
-              query={query}
-              setQuery={setQuery}
-              handleSearch={handleSearch}
-              isLoading={isLoading}
-            />
-            <Results results={results} />
-          </Box>
-        </Box>
-      </Container>
-      <Snackbar
-        open={showNotification}
-        autoHideDuration={6000}
-        onClose={handleCloseNotification}
-        anchorOrigin={{ vertical: 'center', horizontal: 'center' }}
-      >
-        <Alert
-          onClose={handleCloseNotification}
-          severity="info"
-          sx={{ width: '100%' }}
-          action={
-            <Button color="inherit" size="small" onClick={handleOpenGoogle}>
-              Buscar en Google
-            </Button>
-          }
-        >
-          <Box>
-            <Typography>
-              No se encontraron resultados para "{query}".
+      <div className={styles.appContainer}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Menu
             </Typography>
-            <Typography variant="caption">
-              (Las búsquedas son más efectivas con el título original)
-            </Typography>
-          </Box>
-        </Alert>
-      </Snackbar>
+          </Toolbar>
+        </AppBar>
+        <Sidebar open={drawerOpen} toggleDrawer={toggleDrawer} />
+        <main className={styles.container}>
+          <Container className={styles.container}>
+            <header className={styles.appHeader}>
+              <FontAwesomeIcon
+                icon={faReact}
+                size="6x"
+                className={styles.appLogo}
+              />
+              <Welcome />
+              <Typography variant="body1" component="p">
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => setCount((count) => count + 1)}
+                >
+                  count is: {count}
+                </Button>
+              </Typography>
+              <Typography variant="body1" component="p">
+                Edit <code>App.tsx</code> and save to test HMR updates.
+              </Typography>
+              <Typography variant="body1" component="p">
+                <Link
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Learn React
+                </Link>
+                {' | '}
+                <Link
+                  href="https://vitejs.dev/guide/features.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Vite Docs
+                </Link>
+              </Typography>
+              <Switch>
+                <Route path="/about">
+                  <main>About</main>
+                </Route>
+              </Switch>
+            </header>
+          </Container>
+        </main>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
